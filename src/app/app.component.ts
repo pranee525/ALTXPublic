3d3b768e832e14c9ba0028f4961c39701908784e
smhiri <164007913+smhiri@users.noreply.github.com>
2024-04-23 14:06:37 +0200

    firs commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'altex';
+submitText:any="";
+responseArray:any=[];
+responseText:any="";
+
+  constructor(private http: HttpClient) {}
+  clearText(){
+console.log("clear text");
+    this.submitText="";
+
+
+  };
+
+  clearResponse(){
+    this.responseText="";
+    this.responseArray=[];
+  }
+
+
+  
+  sendMessage(message: string) {
+    this.http.post('http://localhost:3000/message', { prompt: message }).subscribe((response: any) => {
+  
+      this.responseArray.push({
+        type: 'response',
+        message: response.message,
+      });
+      this.responseText=this.responseArray[0].message;
+    });
+
+
+  }
+}
